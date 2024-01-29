a9b7a53db29e47f0ed419e6017e31447d0c7f392
Wicho120 <wgalvis1228@gmail.com>
2024-01-29 18:28:37 -0500

    hola

--- PythonClases/clasePython2/ejerciciosTarea.py
+++ PythonClases/clasePython2/ejerciciosTarea.py
@@ -1,3 +1,5 @@
+import random
+
 #ejercicio 1
 
 numIngresado = eval(input("Por favor ingrese un numero para validar si es positivo, negativo o cero: "))
@@ -22,4 +24,98 @@ else:
 
 #ejercicio 3
     
+palabraPalin = input("Por favor ingrese una palabra para verificar si es palindromo o no: ")
+
+if palabraPalin == palabraPalin[::-1]:
+    print("La palabra es un palindromo")
+else:
+    print("La palabra no es un palindromo")
+
+#ejercicio 4 
     
+palabraEvaluar = input("Por favor ingrese un texto para ver el numero de vocales y consonantes: ")
+contVocales = 0
+contConsonantes = 0
+
+for i in palabraEvaluar:
+    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+        contVocales += 1
+    elif i != " ":
+        contConsonantes += 1 +
+
+print("El texto ingresado tiene ", contConsonantes, " consonantes y tiene ", contVocales, " vocales")
+
+
+#ejercicio 5
+
+edadPerro = int(input("Por favor ingrese la edad de su perro: "))
+
+print("La edad de su perro en años de humano es ", (((edadPerro-1)*4)+6))
+
+#ejercicio 6 
+
+print("Bienvenido al juego del dado\n")
+
+while True:
+    numDado = random.randint(1,6)
+    
+    if numDado == 6:
+        print("Felicitaciones el dado cayó en el 6")
+        break
+    else:
+        print("El numero que cayó fue ", numDado)
+
+#ejercicio 7
+        
+print("Vamos a jugar a piedra papel o tijera")
+
+opciones = ["Piedra", "Papel", "tijera"]
+
+
+tiroMaquina = random.randint(0, 2)
+
+tiroUsuario = int(input("Por favor ingrese su eleccion \n1. Piedra\n2. Papel\n3. Tijera\n"))
+
+if tiroUsuario == tiroMaquina:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nFue un empate")
+elif tiroUsuario == 1 and tiroMaquina == 2:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nGano la maquina")
+elif tiroUsuario == 1 and tiroMaquina == 3:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nGano el usuario")
+elif tiroUsuario == 2 and tiroMaquina == 1:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nGano el usuario")
+elif tiroUsuario == 2 and tiroMaquina == 3:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nGano la maquina")
+elif tiroUsuario == 3 and tiroMaquina == 1:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nGano la maquina")
+elif tiroUsuario == 3 and tiroMaquina == 2:
+    print("Maquina: ", opciones[tiroMaquina], " Usuario: ", opciones[tiroUsuario-1], "\nGano el usuario")
+
+
+#ejercicio 8
+
+numStop = int(input("Por favor ingrese la cantidad de numeros de fibonacci que quiere ver: "))
+
+numFibonacci1 = 0
+numFibonacci2 = 0
+numFibonacci3 = 1
+
+print(numFibonacci2)
+print(numFibonacci3)
+
+for i in range(numStop-2):
+    numFibonacci1 = numFibonacci2 + numFibonacci3
+    print(numFibonacci1)
+    numFibonacci2 = numFibonacci3
+    numFibonacci3 = numFibonacci1
+
+#ejercicio 9
+    
+opcion = int(input("Bienvenido al conversor de temperatura, que desea hacer.\n1. Celsius a Fahrenheit\n2. Fahrenheit a Celsius"))
+
+if opcion == 1:
+    temp = int(input("Por favor ingrese la temperatura en Celsius: "))
+    print("La temperatura al pasarla a Fahrenheit es: ", (temp*33.8)) 
+elif opcion ==2:
+    temp = int(input("Por favor ingrese la temperatura en Fahrenheit: "))
+    print("La temperatura al pasarla a Celsius es: ", (temp/33.8))    
